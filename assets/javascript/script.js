7ef4b424ee914a3febec8a6fbb912f74f25ef050
Howard Daniels <hdaniels13tat@gmail.com>
2019-12-08 12:53:07 -0500

    added moment.js for the title and subtitle, and added Bootstrap inputs and buttons into the calendar

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,2 +1,6 @@
-var d = new Date();
+var d = moment().format('LL');
 $("#today").text(d);
+var wd = moment().format('dddd');
+$("#weekday").text(wd);
+var t = moment().format('LTS');
+$("#time").text(t);
