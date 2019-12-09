1ee08d66f999a5b3a0712e13773ba7e0fe313d54
Howard Daniels <hdaniels13tat@gmail.com>
2019-12-09 16:26:01 -0500

    replicated the event listeners on the first row onto the remaining rows

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -17,3 +17,115 @@ document.querySelector("#save").addEventListener("click", function(){
     document.getElementById("notes9").setAttribute("style", "background-color: green;");
         document.getElementById("nineam").setAttribute("style", "background-color: green;");
 });
+
+
+document.querySelector('#tenam').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { 
+        document.getElementById("notes10").setAttribute("style", "background-color: red;");
+        document.getElementById("tenam").setAttribute("style", "background-color: red; font-weight: bold;");
+  }
+});
+
+document.querySelector("#save").addEventListener("click", function(){
+    document.getElementById("notes10").setAttribute("style", "background-color: green;");
+        document.getElementById("tenam").setAttribute("style", "background-color: green;");
+});
+
+
+document.querySelector('#elevenam').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { 
+        document.getElementById("notes11").setAttribute("style", "background-color: red;");
+        document.getElementById("elevenam").setAttribute("style", "background-color: red; font-weight: bold;");
+  }
+});
+
+document.querySelector("#save").addEventListener("click", function(){
+    document.getElementById("notes11").setAttribute("style", "background-color: green;");
+        document.getElementById("elevenam").setAttribute("style", "background-color: green;");
+});
+
+
+document.querySelector('#twelvepm').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { 
+        document.getElementById("notes12").setAttribute("style", "background-color: red;");
+        document.getElementById("twelvepm").setAttribute("style", "background-color: red; font-weight: bold;");
+  }
+});
+
+document.querySelector("#save").addEventListener("click", function(){
+    document.getElementById("notes12").setAttribute("style", "background-color: green;");
+        document.getElementById("twelvepm").setAttribute("style", "background-color: green;");
+});
+
+
+document.querySelector('#onepm').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { 
+        document.getElementById("notes13").setAttribute("style", "background-color: red;");
+        document.getElementById("onepm").setAttribute("style", "background-color: red; font-weight: bold;");
+  }
+});
+
+document.querySelector("#save").addEventListener("click", function(){
+    document.getElementById("notes13").setAttribute("style", "background-color: green;");
+        document.getElementById("onepm").setAttribute("style", "background-color: green;");
+});
+
+
+document.querySelector('#twopm').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { 
+        document.getElementById("notes14").setAttribute("style", "background-color: red;");
+        document.getElementById("twopm").setAttribute("style", "background-color: red; font-weight: bold;");
+  }
+});
+
+document.querySelector("#save").addEventListener("click", function(){
+    document.getElementById("notes14").setAttribute("style", "background-color: green;");
+        document.getElementById("twopm").setAttribute("style", "background-color: green;");
+});
+
+
+document.querySelector('#threepm').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { 
+        document.getElementById("notes15").setAttribute("style", "background-color: red;");
+        document.getElementById("threepm").setAttribute("style", "background-color: red; font-weight: bold;");
+  }
+});
+
+document.querySelector("#save").addEventListener("click", function(){
+    document.getElementById("notes15").setAttribute("style", "background-color: green;");
+        document.getElementById("threepm").setAttribute("style", "background-color: green;");
+});
+
+
+document.querySelector('#fourpm').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { 
+        document.getElementById("notes16").setAttribute("style", "background-color: red;");
+        document.getElementById("fourpm").setAttribute("style", "background-color: red; font-weight: bold;");
+  }
+});
+
+document.querySelector("#save").addEventListener("click", function(){
+    document.getElementById("notes16").setAttribute("style", "background-color: green;");
+        document.getElementById("fourpm").setAttribute("style", "background-color: green;");
+});
+
+
+document.querySelector('#fivepm').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { 
+        document.getElementById("notes17").setAttribute("style", "background-color: red;");
+        document.getElementById("fivepm").setAttribute("style", "background-color: red; font-weight: bold;");
+  }
+});
+
+document.querySelector("#save").addEventListener("click", function(){
+    document.getElementById("notes17").setAttribute("style", "background-color: green;");
+        document.getElementById("fivepm").setAttribute("style", "background-color: green;");
+});
