e94bd7b6fbdae0c0fe4c3c0819145bd0810fa9f6
Howard Daniels <hdaniels13tat@gmail.com>
2019-12-08 23:00:22 -0500

    set up event listeners on input and save button for 9 am row

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,3 +4,16 @@ var wd = moment().format('dddd');
 $("#weekday").text(wd);
 var t = moment().format('LTS');
 $("#time").text(t);
+
+document.querySelector('#nineam').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { 
+        document.getElementById("notes9").setAttribute("style", "background-color: red;");
+        document.getElementById("nineam").setAttribute("style", "background-color: red; font-weight: bold;");
+  }
+});
+
+document.querySelector("#save").addEventListener("click", function(){
+    document.getElementById("notes9").setAttribute("style", "background-color: green;");
+        document.getElementById("nineam").setAttribute("style", "background-color: green;");
+});
