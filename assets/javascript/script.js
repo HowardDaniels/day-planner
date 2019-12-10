ece2da2c38bc8379f45d5e9447dea54d56ba4b68
Howard Daniels <hdaniels13tat@gmail.com>
2019-12-10 13:49:57 -0500

    set color scheme for 2 pm

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,9 +9,9 @@ var t24 = moment().format();
 var h = parseInt(t24.substring(11, Math.min(t24.length, 13)));
 */
 
-var h = 13;
+var h = 14;
 
-if ((h < 9) || (h > 13)){
+if ((h < 9) || (h > 14)){
 alert("yeah");
 }
 
@@ -58,6 +58,21 @@ else if (h == 13){
     document.getElementById("twelvepm").setAttribute("style", "background-color: gray; color: white;");
     document.getElementById("notes13").setAttribute("style", "background-color: lightgreen;");
     document.getElementById("onepm").setAttribute("style", "background-color: lightgreen;");
+}
+
+else if (h == 14){
+    document.getElementById("notes9").setAttribute("style", "background-color: gray;");
+    document.getElementById("nineam").setAttribute("style", "background-color: gray; color: white;");
+    document.getElementById("notes10").setAttribute("style", "background-color: gray;");
+    document.getElementById("tenam").setAttribute("style", "background-color: gray; color: white;");
+    document.getElementById("notes11").setAttribute("style", "background-color: gray;");
+    document.getElementById("elevenam").setAttribute("style", "background-color: gray; color: white;");
+    document.getElementById("notes12").setAttribute("style", "background-color: gray;");
+    document.getElementById("twelvepm").setAttribute("style", "background-color: gray; color: white;");
+    document.getElementById("notes13").setAttribute("style", "background-color: gray;");
+    document.getElementById("onepm").setAttribute("style", "background-color: gray; color: white;");
+    document.getElementById("notes14").setAttribute("style", "background-color: lightgreen;");
+    document.getElementById("twopm").setAttribute("style", "background-color: lightgreen;");
 }
 /*
 
